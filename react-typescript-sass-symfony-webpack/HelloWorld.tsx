8dde37925244d88316a5aa4f889659360e4b7ff9
Raheny Roger <heniroger@yahoo.fr>
2021-11-02 18:43:55 +0300

    Add files via upload

---
+++ react-typescript-sass-symfony-webpack/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class HelloWorld extends Component
+{
+    
+    render() {
+        
+        return <h1>Hello, world!</h1>
+    }
+}
---
+++ react-typescript-sass-symfony-webpack/app.tsx
@@ -0,0 +1,29 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+//import './bootstrap/js/dist/util';
+//import './bootstrap/js/dist/dropdown';
+//import * as ReactBootstrap from 'react-bootstrap';
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import HelloWorld from './HelloWorld';
+
+//ReactDOM.render(<BrowserRouter><HelloWorld /></BrowserRouter>, document.querySelector('#root'));
+
